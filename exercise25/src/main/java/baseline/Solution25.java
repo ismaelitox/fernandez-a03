e2baab17eea709886477619f9ead4568588aef11
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-03 01:59:12 -0400

    psuedo/skele

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,33 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution25 {
+    // Scanner Object
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Need prompt for asking for password
+        // probably will use regex to sort through characters again
+        // possible separate methods to see if its weak, strong ,very strong
+        // if not probably will have single functions with cases and breaks
+        // or maybe else if
+
+        // method defined as shall
+        // can return true or false with prompts
+        // or cases for each
+        public static boolean passwordValidator(String password)
+        {
+
+        }
+        // Method to get password
+        // or may just keep it in main
+        public static String getPassword(){
+
+
+        }
+
+
+    }
+}
