532900799bc426710f6d7824ebea93020c92c4ba
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-02 21:21:43 -0400

    psuedoCode

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,37 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Jonah Fernandez
+ */
+
+import java.util.Scanner;
+
+public class Solution24 {
+    // Scanner object
+    private final static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Method to get Anagrams
+        // Method to compare strings of the anagrams
+        // Use a boolean method to compare strings possibly in an array?
+        // an array sort method possibly
+        String s1 = getAnagramStrings();
+        String s2 = getAnagramStrings();
+
+
+    }
+
+    private static String getAnagramStrings() {
+        // Method used for getting string 1
+        // and string to and returning to main
+        return "";
+    }
+
+    private static void anagramChecker(String s1, String s2){
+        // Method using boolean true false to check
+        // comparing length of both strings
+        // if strings arent equal return false.
+        // if strings have the same contents return true
+    }
+
+}
