0eeea0742022c6a92a0e9b619acc14d17f9afd03
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-02 23:56:39 -0400

    needs test cases

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,35 +4,56 @@ package baseline;
  *  Copyright 2021 Jonah Fernandez
  */
 
+// Going to have to w
+import java.text.MessageFormat;
+import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Solution24 {
     // Scanner object
-    private final static Scanner in = new Scanner(System.in);
+    private static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         // Method to get Anagrams
         // Method to compare strings of the anagrams
         // Use a boolean method to compare strings possibly in an array?
         // an array sort method possibly
-        String s1 = getAnagramStrings();
-        String s2 = getAnagramStrings();
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
 
+        System.out.print("Enter the first string: ");
+        String str1 = in.nextLine();
 
+        System.out.print("Enter the second string: ");
+        String str2 = in.nextLine();
+        // regex to remove all whitespace
+        str1 = str1.replaceAll("\\s","").toLowerCase();
+        str2 = str2.replaceAll("\\s","").toLowerCase();
+
+         if(isAnagram(str1,str2))
+             System.out.println(MessageFormat.format("\"{0}\" and \"{1}\" are anagrams.",str1,str2));
     }
 
-    private static String getAnagramStrings() {
-        // Method used for getting string 1
-        // and string to and returning to main
+    // Method using boolean true false to check
+    // comparing length of both strings
+    // if strings arent equal return false.
+    // if strings have the same contents return true
+    // using arrays to compare elements inside of arrays with .sort java function
+        private static boolean isAnagram(String str1, String str2){
+            if(str1.length() != str2.length())
+                return false;
+            else
+            {
+                char[] str1Array = str1.toCharArray();
+                char[] str2Array = str2.toCharArray();
 
-        return "";
-    }
+                Arrays.sort(str1Array);
+                Arrays.sort(str2Array);
 
-    private static void anagramChecker(String s1, String s2){
-        // Method using boolean true false to check
-        // comparing length of both strings
-        // if strings arent equal return false.
-        // if strings have the same contents return true
-    }
+                return Arrays.equals(str1Array,str2Array);
+            }
+
+        }
+
 
 }
