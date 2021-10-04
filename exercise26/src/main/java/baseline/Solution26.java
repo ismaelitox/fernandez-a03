70f9f06a8e03815712826fb9b0f505415d0d13a3
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-03 22:12:32 -0400

    psuedoskele

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,31 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Jonah Fernandez
+ */
+
+import java.util.Scanner;
+
+public class Solution26 {
+    // Scanner Object
+    private static final Scanner in = new Scanner(System.in);
+
+    // requirements asking for program to pay off credit card
+    // how many months will it take to pay off
+    // prompt asking for balance
+    // prompt asking for APR
+    // prompt asking for the monthly payment
+    // return int number of months needed with ciel
+
+    // declare seprate method to do calculations
+    // use formula: n = -(1/30) * log(1 +b/p * (1 - (1 + i)^30)) / log(1 +i)
+    // n is number of months
+    // i is daily rate APR/365
+    // p is the monthly payment
+}
+class PaymentCalculator{
+
+    public int calculateMonthsUntilPaidOff(){
+        return 0;
+    }
+}
