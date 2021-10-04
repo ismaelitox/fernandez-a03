0da3258699ae2b5f45c6a2dc72aca78b79bc227d
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-03 23:54:37 -0400

    yu

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution27 {
+
+    public static void main(String[] args) {
+        // Validation of inputs
+        // probably going to use some form of regex to validate the strings again
+        // prompt for first name
+        // prompt for last name
+        // prompt for zipcode
+        // prompt for employee ID
+        // regex all strings checks for correct inputs
+
+    }
+    private String validateInput(){
+        return "";
+    }
+}
